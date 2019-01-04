7f929e914f486a15176e4a479433444774246e21
nlsui <marius@itc-faust,de>
2019-01-04 17:12:07 +0100

    Revert "made zoneChange-struct public"
    This reverts commit 7f1be1c63c731ee487729c5a0287a2a6159ef6e9.

--- zoneChange.go
+++ zoneChange.go
@@ -1,19 +1,25 @@
 package cardobject
 
-func NewZoneChange(s CardSelectorSafeCond, z Zone) *ZoneChange {
-	return &ZoneChange{s, z}
+type ZoneChange interface {
+	GetSelection() CardSelectorSafeCond
+	GetDestination() Zone
 }
 
-type ZoneChange struct {
+func NewZoneChange(s CardSelectorSafeCond, z Zone) ZoneChange {
+	return &zoneChange{s, z}
+}
+
+
+type zoneChange struct {
 	selection CardSelectorSafeCond
 	destination Zone
 }
 
 
-func (zc *ZoneChange) GetSelection() CardSelectorSafeCond {
+func (zc *zoneChange) GetSelection() CardSelectorSafeCond {
 	return zc.selection
 }
 
-func (zc *ZoneChange) GetDestination() Zone {
+func (zc *zoneChange) GetDestination() Zone {
 	return zc.destination
 }
