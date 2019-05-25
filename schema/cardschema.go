42ca1b35194cee306246cc343dcac8d435d61b4e
unknown <marius@itc-faust.de>
2019-05-25 12:33:46 +0200

    added schema

---
+++ schema/cardschema.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "github.com/xeipuuv/gojsonschema"
+
+func main() {
+
+    schemaLoader := gojsonschema.NewReferenceLoader("file://C:/Users/marius/Documents/goworkspace/src/github.com/DecentralCardGame/cardobject/schema/cardschema.json")
+    documentLoader := gojsonschema.NewReferenceLoader("file://C:/Users/marius/Documents/goworkspace/src/github.com/DecentralCardGame/cardobject/schema/card.json")
+
+    result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    if result.Valid() {
+        fmt.Printf("The document is valid\n")
+    } else {
+        fmt.Printf("The document is not valid. see errors :\n")
+        for _, desc := range result.Errors() {
+            fmt.Printf("- %s\n", desc)
+        }
+    }
+}
