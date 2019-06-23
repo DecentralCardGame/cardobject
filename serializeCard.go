5575adc50a52f1c7fabd44babc08a2afd881ee51
unknown <marius@itc-faust.de>
2019-06-23 13:54:35 +0200

    update

--- serializeCard.go
+++ serializeCard.go
@@ -2,8 +2,23 @@ package cardobject
 
 import "fmt"
 import "encoding/json"
+import "github.com/xeipuuv/gojsonschema"
+import amino "github.com/tendermint/go-amino"
 
-func SerializeCard(c Card) string {
+func ProcessCard (cardJson string) string {
+	cdc := amino.NewCodec()
+	registerCodec(cdc)
+	var card card
+	cdc.MustUnmarshalJSON([]byte(cardJson), &card)
+
+	if(validateCard(cardJson)) {
+		return serializeCard(deserializeCard(cardJson))
+	} else {
+		return ""
+	}
+}
+
+func serializeCard(c card) string {
 	bytes, err := json.Marshal(c)
     if err != nil {
         fmt.Println("Can't serialize", c)
@@ -12,6 +27,38 @@ func SerializeCard(c Card) string {
     return string(bytes)
 }
 
-func CreateAction() Action {
-	return &action{}
+func deserializeCard(s string) card {
+	var card entity
+	err := json.Unmarshal([]byte(s), &card)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return card
+}
+
+func validateCard(s string) bool {
+    schemaLoader := gojsonschema.NewReferenceLoader("file://C:/Users/marius/Documents/goworkspace/src/github.com/DecentralCardGame/cardobject/schema/cardSchema.json")
+    documentLoader := gojsonschema.NewStringLoader(s)
+
+    result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    if result.Valid() {
+        fmt.Printf("The document is valid\n")
+        return true
+    } else {
+        fmt.Printf("The document is not valid. see errors :\n")
+        for _, desc := range result.Errors() {
+            fmt.Printf("- %s\n", desc)
+        }
+        return false
+    }
+}
+
+func registerCodec(cdc *amino.Codec) {
+	cdc.RegisterInterface((*card)(nil), nil)
+	cdc.RegisterConcrete(action{}, "action", nil)
+	cdc.RegisterConcrete(entity{}, "entity", nil)
 }
--- serializeCard_test.go
+++ serializeCard_test.go
@@ -2,8 +2,21 @@ package cardobject
 
 import "testing"
 import "fmt"
+import "io/ioutil"
+import amino "github.com/tendermint/go-amino"
 
 func TestCardSerialization(t *testing.T) {
-	x := createAction()
-	fmt.Println(SerializeCard(x))
+	file, _ := ioutil.ReadFile("schema/entity.json")
+ 	fmt.Println(ProcessCard(string(file)))
+ 	var cardBuffer card
+
+	cdc := amino.NewCodec()
+	cdc.RegisterInterface((*card)(nil), nil)
+	cdc.RegisterConcrete(action{}, "action", nil)
+	cdc.RegisterConcrete(entity{}, "entity", nil)
+
+	//json, _ := cdc.MarshalJSON(action{})
+
+	cdc.MustUnmarshalJSON([]byte(file), &cardBuffer)
+
 }
