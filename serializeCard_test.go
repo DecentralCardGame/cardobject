69d6aa250365e06733c24ff691dab42eb2f0345e
unknown <marius@itc-faust.de>
2019-08-05 21:05:05 +0200

    more tests

--- serializeCard_test.go
+++ serializeCard_test.go
@@ -5,10 +5,38 @@ import "fmt"
 import "io/ioutil"
 import "strings"
 
-func TestCardSerialization(t *testing.T) {
+func TestCardSerialization1(t *testing.T) {
 	file, _ := ioutil.ReadFile("schema/entity2.json")
 	input := string(file)
 	output := ProcessCard(input)
- 	fmt.Println(output)
+ 	//fmt.Println(output)
+ 	fmt.Println(strings.Compare(input, output))
+}
+func TestCardSerialization2(t *testing.T) {
+	file, _ := ioutil.ReadFile("schema/actionTest.json")
+	input := string(file)
+	output := ProcessCard(input)
+ 	//fmt.Println(output)
+ 	fmt.Println(strings.Compare(input, output))
+}
+func TestCardSerialization3(t *testing.T) {
+	file, _ := ioutil.ReadFile("schema/fieldTest.json")
+	input := string(file)
+	output := ProcessCard(input)
+ 	//fmt.Println(output)
+ 	fmt.Println(strings.Compare(input, output))
+}
+func TestCardSerialization4(t *testing.T) {
+	file, _ := ioutil.ReadFile("schema/hqTest.json")
+	input := string(file)
+	output := ProcessCard(input)
+ 	//fmt.Println(output)
+ 	fmt.Println(strings.Compare(input, output))
+}
+func TestCardSerialization5(t *testing.T) {
+	file, _ := ioutil.ReadFile("schema/entityTest.json")
+	input := string(file)
+	output := ProcessCard(input)
+ 	//fmt.Println(output)
  	fmt.Println(strings.Compare(input, output))
 }
