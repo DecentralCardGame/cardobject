4116d7f1ec1251e662684d9093b556834ffccec2
unknown <marius@itc-faust.de>
2019-08-13 13:47:25 +0200

    added testfolder

--- serializeCard_test.go
+++ serializeCard_test.go
@@ -6,28 +6,28 @@ import "io/ioutil"
 import "strings"
 
 func TestCardSerialization2(t *testing.T) {
-	file, _ := ioutil.ReadFile("schema/actionTest.json")
+	file, _ := ioutil.ReadFile("testJsons/actionTest.json")
 	input := string(file)
 	output := ProcessCard(input)
  	//fmt.Println(output)
  	fmt.Println(strings.Compare(input, output))
 }
 func TestCardSerialization3(t *testing.T) {
-	file, _ := ioutil.ReadFile("schema/fieldTest.json")
+	file, _ := ioutil.ReadFile("testJsons/fieldTest.json")
 	input := string(file)
 	output := ProcessCard(input)
  	//fmt.Println(output)
  	fmt.Println(strings.Compare(input, output))
 }
 func TestCardSerialization4(t *testing.T) {
-	file, _ := ioutil.ReadFile("schema/hqTest.json")
+	file, _ := ioutil.ReadFile("testJsons/hqTest.json")
 	input := string(file)
 	output := ProcessCard(input)
  	//fmt.Println(output)
  	fmt.Println(strings.Compare(input, output))
 }
 func TestCardSerialization5(t *testing.T) {
-	file, _ := ioutil.ReadFile("schema/entityTest.json")
+	file, _ := ioutil.ReadFile("testJsons/entityTest.json")
 	input := string(file)
 	output := ProcessCard(input)
  	//fmt.Println(output)
