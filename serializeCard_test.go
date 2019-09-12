8508002b66e2fc377f2b1f3eff2873eb28d5cfb1
unknown <marius@itc-faust.de>
2019-09-12 19:44:48 +0200

    finalized tests

--- serializeCard_test.go
+++ serializeCard_test.go
@@ -4,8 +4,8 @@ import "testing"
 import "io/ioutil"
 import "strings"
 
-func TestCardSerialization2(t *testing.T) {
-	file, _ := ioutil.ReadFile("testJsons/actionTest.json")
+func TestCardSerializationAction1(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/action1Test.json")
 	input := string(file)
 	output, err := ProcessCard(input)
 	if err != nil {
@@ -15,8 +15,8 @@ func TestCardSerialization2(t *testing.T) {
 		t.Errorf("In- and output are not equal")
 	}
 }
-func TestCardSerialization3(t *testing.T) {
-	file, _ := ioutil.ReadFile("testJsons/fieldTest.json")
+func TestCardSerializationAction2(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/action2Test.json")
 	input := string(file)
 	output, err := ProcessCard(input)
 	if err != nil {
@@ -26,8 +26,8 @@ func TestCardSerialization3(t *testing.T) {
 		t.Errorf("In- and output are not equal")
 	}
 }
-func TestCardSerialization4(t *testing.T) {
-	file, _ := ioutil.ReadFile("testJsons/hqTest.json")
+func TestCardSerializationAction3(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/action3Test.json")
 	input := string(file)
 	output, err := ProcessCard(input)
 	if err != nil {
@@ -37,8 +37,96 @@ func TestCardSerialization4(t *testing.T) {
 		t.Errorf("In- and output are not equal")
 	}
 }
-func TestCardSerialization5(t *testing.T) {
-	file, _ := ioutil.ReadFile("testJsons/entityTest.json")
+func TestCardSerializationField1(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/field1Test.json")
+	input := string(file)
+	output, err := ProcessCard(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if(strings.Compare(input, output) != 0){
+		t.Errorf("In- and output are not equal")
+	}
+}
+func TestCardSerializationField2(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/field2Test.json")
+	input := string(file)
+	output, err := ProcessCard(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if(strings.Compare(input, output) != 0){
+		t.Errorf("In- and output are not equal")
+	}
+}
+func TestCardSerializationField3(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/field3Test.json")
+	input := string(file)
+	output, err := ProcessCard(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if(strings.Compare(input, output) != 0){
+		t.Errorf("In- and output are not equal")
+	}
+}
+func TestCardSerializationHq1(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/hq1Test.json")
+	input := string(file)
+	output, err := ProcessCard(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if(strings.Compare(input, output) != 0){
+		t.Errorf("In- and output are not equal")
+	}
+}
+func TestCardSerializationHq2(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/hq2Test.json")
+	input := string(file)
+	output, err := ProcessCard(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if(strings.Compare(input, output) != 0){
+		t.Errorf("In- and output are not equal")
+	}
+}
+func TestCardSerializationHq3(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/hq3Test.json")
+	input := string(file)
+	output, err := ProcessCard(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if(strings.Compare(input, output) != 0){
+		t.Errorf("In- and output are not equal")
+	}
+}
+func TestCardSerializationEntity1(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/entity1Test.json")
+	input := string(file)
+	output, err := ProcessCard(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if(strings.Compare(input, output) != 0){
+		t.Errorf("In- and output are not equal")
+	}
+}
+func TestCardSerializationEntity2(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/entity2Test.json")
+	input := string(file)
+	output, err := ProcessCard(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if(strings.Compare(input, output) != 0){
+		t.Errorf("In- and output are not equal")
+	}
+}
+func TestCardSerializationEntity3(t *testing.T) {
+	file, _ := ioutil.ReadFile("testJsons/entity3Test.json")
 	input := string(file)
 	output, err := ProcessCard(input)
 	if err != nil {
