3b5cc7041ac2ce5c0f9a90ef77566b20907ed804
nlsui <37106890+nlsui@users.noreply.github.com>
2019-12-06 17:23:24 +0100

    added ability manipulation

--- manipulation.go
+++ manipulation.go
@@ -1,5 +1,10 @@
 package cardobject
 
+type abilityManipulationBasics struct{
+	AbilityOperator string
+	Ability ability
+}
+
 type intManipulationBasics struct {
 	IntProperty string
 	IntOperator string
@@ -18,11 +23,13 @@ type actionManipulation struct {
 }
 
 type entityManipulation struct {
+	EntityAbilityManipulation *abilityManipulationBasics `json:",omitempty"`
 	EntityIntManipulation *intManipulationBasics `json:",omitempty"`
 	EntityStringManipulation *stringManipulationBasics `json:",omitempty"`
 }
 
 type locationManipulation struct {
+	LocationAbilityManipulation *abilityManipulationBasics `json:",omitempty"`
 	LocationIntManipulation *intManipulationBasics `json:",omitempty"`
 	LocationStringManipulation *stringManipulationBasics `json:",omitempty"`
 }
