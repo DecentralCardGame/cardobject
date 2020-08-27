11c7d10968221f36db4041746ecf6a4b11225d4d
nlsui <37106890+nlsui@users.noreply.github.com>
2020-08-27 12:48:39 +0200

    removed unused stuff

--- token.go
+++
@@ -1,9 +0,0 @@
-package cardobject
-
-type token struct {
-	Name         *string `json:",omitempty"`
-	Tag          *string `json:",omitempty"`
-	AbilitySpeed int8
-	Health       int8
-	Attack       int8
-}
