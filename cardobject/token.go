6185a0e97ec0b7549ea30c787493f513c1b73b0f
nlsui <37106890+nlsui@users.noreply.github.com>
2021-05-08 19:36:25 +0200

    added tokentypes

--- cardobject/token.go
+++ cardobject/token.go
@@ -1,6 +1,42 @@
 package cardobject
 
-import "github.com/DecentralCardGame/cardobject/jsonschema"
+import (
+	"errors"
+	"strings"
+
+	"github.com/DecentralCardGame/cardobject/jsonschema"
+)
+
+//Recruit Token
+const Recruit = "1/1 human Recruit"
+
+//Bot Token
+const Bot = "2/2 Bot"
+
+//Beast Token
+const Beast = "3/3 animal Beast"
+
+var tokenTypes []string = []string{Recruit, Bot, Beast}
+
+type TokenType jsonschema.BasicEnum
+
+func (t TokenType) Validate() error {
+	return t.ValidateEnum()
+}
+
+func (t TokenType) ValidateEnum() error {
+	values := t.EnumValues()
+	for _, t := range values {
+		if t == string(t) {
+			return nil
+		}
+	}
+	return errors.New("TokenType must be one of: " + strings.Join(tokenTypes, ","))
+}
+
+func (t TokenType) EnumValues() []string {
+	return tokenTypes
+}
 
 type Token struct {
 	Name   CardName `json:",omitempty"`
